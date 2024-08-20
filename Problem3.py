8139432e7ec2880580f7d482054aa5f408179664
MJMcNevin <71196609+MJMcNevin@users.noreply.github.com>
2024-08-20 02:27:50 -0700

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -2,48 +2,34 @@
 #What is the largest prime factor of the number 600851475143.
 
 import time;
-import math;
-isPrime=False;
 
 def timed (n):
     start = time.time();
     result = largestPrimeFactor(int(n));
     end = time.time();
-    duration = float(end) - float(start);
-    print('Largest prime factor of {} is {}. Execution: {} seconds'.format(n, result, duration));
-
+    duration = float(end) - float(start); 
+    if result==None:
+        print(n,'has no prime factor.');
+    else:
+        print('Largest prime factor of {} is {}. Execution: {}.'.format(n, result,duration));    
+ 
 def largestPrimeFactor(number):
-    #number=int(number);
-    factors=[]
-    for i in range(2,int(math.sqrt(number))):
-        if primeCheck(i):
-            if number%i==0:
-                factors.append(i);
-    return factors[-1];
-    
-    
-    
-  
+    if number>1:
+        for i in range(int(number**0.5),1,-1):
+            if number%i==0 and primeCheck(i):
+                return i;
+    else:
+        return None
+
     
 def primeCheck(number):
-    global isPrime;
-    if number > 1:
-        for i in range(2,number):
-            if (number%i)==0:
-                isPrime=False;                
-                #print (number, 'is not a prime number.');
-                return isPrime;
-        else:
-            isPrime=True;
-            primeNum=number;
-            #print (number, 'is a prime number.');
-            return isPrime;
-            
-             
-    else:
-        isPrime=False;
-        print (number, 'is not a prime number.');
-
+    i=2;
+    while i<number:
+        if (number%i)==0:              
+            return False;                
+        i=i+1;
+    return True;
+    
 
 
 number=input('Number to check: ');
