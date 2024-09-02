0abece7389d5db1198f0f5d3c374abb40fd34993
MJMcNevin <71196609+MJMcNevin@users.noreply.github.com>
2024-09-02 01:23:48 -0700

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -9,6 +9,13 @@ def palCheck(p):
         return False;
 
 
-
-test=input("Number to check: ");
-print (palCheck(test));
+def largestPalFind():
+    largestPal=0;
+    for i in range(100,1000):
+        for j in range(100,1000):
+            n=i*j;
+            if palCheck(str(n)) and n>largestPal:
+                largestPal=n;
+    return largestPal;
+     
+print('The largest palindrome made from the product of two 3-digit numbers is:',largestPalFind());
